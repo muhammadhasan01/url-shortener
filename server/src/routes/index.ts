f89f9b733e6076d466fa79dbefcc902ce0745a12
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-11-26 20:25:13 +0700

    fix: remove unused import

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,5 @@
 import {Express, Request, Response} from "express";
 import {createShortUrl} from "../controller/shortUrl.controller";
-import bodyParser from "body-parser";
 
 function routes(app: Express) {
   app.get('/health-check', (_: Request, res: Response) => {
