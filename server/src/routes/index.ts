8a0ed7d20f26a13d97f741ce623bdb9a8baf0570
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-11-27 13:20:55 +0700

    feat: add validator middleware on url shortener

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,14 @@
 import {Express, Request, Response} from "express";
 import {createShortUrl, handleRedirect} from "../controller/shortUrl.controller";
+import validateResource from '../middleware/validateResource';
+import shortUrlSchema from '../schemas/createShortUrl.schema';
 
 function routes(app: Express) {
   app.get("/health-check", (_: Request, res: Response) => {
     return res.send("healthy");
   })
 
-  app.post("/url-shortener", createShortUrl);
+  app.post("/url-shortener", validateResource(shortUrlSchema), createShortUrl);
   app.get("/:shortId", handleRedirect);
 }
 
