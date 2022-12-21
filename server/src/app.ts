110f5530cc44a8b6ae04dbdabde52fee6f55846d
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-21 13:43:43 +0700

    feat: add cors into app

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,14 @@ import routes from "./routes";
 import bodyParser from "body-parser";
 import db from "./db";
 import dotenv from "dotenv";
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors())
 app.use(bodyParser.json());
 
 app.listen(port, () => {
