366bb0c281edca8b0c295832567ff3b0d9c7ef57
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-21 13:43:58 +0700

    style: rename to SERVER_ENDPOINT instead

--- ui/src/config/index.ts
+++ ui/src/config/index.ts
@@ -1,1 +1,1 @@
-export const SERVER_ENDPOINTS = process.env.REACT_APP_SERVER_ENDPOINT || 'http://localhost:4000';
+export const SERVER_ENDPOINT = process.env.REACT_APP_SERVER_ENDPOINT || 'http://localhost:4000';
