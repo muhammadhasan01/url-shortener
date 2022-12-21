379919f364523285d3059c459d39c5e575a0fe79
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-21 13:54:52 +0700

    feat: add URL Shortener Form Component

--- ui/src/components/URLShortenerForm.tsx
+++ ui/src/components/URLShortenerForm.tsx
@@ -1,7 +1,39 @@
+import {Input, Button, Box} from "@chakra-ui/react";
+import axios from 'axios';
+import {useState} from "react";
+import {SERVER_ENDPOINT} from "../config";
+
+const PLACEHOLDER = 'https://example.com';
+
 const URLShortenerForm = () => {
-  return <div className='app'>
-    Form
-  </div>
+  const [destination, setDestination] = useState();
+  const [shortUrl, setShortUrl] = useState<{
+    shortId: string;
+  } | null>(null);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log({destination})
+    const result = await axios.post(`${SERVER_ENDPOINT}/api/url-shortener`, {
+      destination
+    }).then(res => res.data)
+    setShortUrl(result)
+  }
+
+  return <Box pos="relative">
+    <form onSubmit={handleSubmit}>
+      <Input
+        onChange={(e: any) => setDestination(e.target.value)}
+        placeholder={PLACEHOLDER}
+      />
+      <Button type="submit">Create Short URL</Button>
+    </form>
+    {shortUrl && (
+      <a href={`${SERVER_ENDPOINT}/${shortUrl.shortId}`}>
+        CLICK ME
+      </a>
+    )}
+  </Box>
 }
 
 export default URLShortenerForm;
