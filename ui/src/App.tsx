e3abc295144f2e4b868d059244741bc6df69bdb2
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-21 16:31:53 +0700

    feat: use Box container in app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,18 @@
 import URLShortenerForm from "./components/URLShortenerForm";
 import Background from "./components/Background";
+import {Box} from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <URLShortenerForm />
-      <Background />
-    </div>
+    <Box
+      height="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <URLShortenerForm/>
+      <Background/>
+    </Box>
   );
 }
 
