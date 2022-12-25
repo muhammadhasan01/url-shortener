a15cff6a43e630daaad74d3718109241699b8952
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-25 13:23:34 +0700

    feat: implement HandleRedirect.tsx

--- ui/src/containers/HandleRedirect.tsx
+++ ui/src/containers/HandleRedirect.tsx
@@ -1,7 +1,47 @@
+import axios from 'axios';
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {Spinner, Box} from "@chakra-ui/react";
+import {SERVER_ENDPOINT} from "../config";
+
 const HandleRedirect = () => {
-  return <div>
-    redirect
-  </div>
+  const [destination, setDestination] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const {shortId} = useParams();
+
+  console.log({shortId});
+
+  useEffect(() => {
+    const getData = async () => {
+      return axios.get(`${SERVER_ENDPOINT}/api/destination/${shortId}`)
+        .then(res => setDestination(res.data.destination))
+        .catch(err => setError(err))
+    }
+    getData();
+  }, [shortId]);
+
+  useEffect(() => {
+    if (!destination) {
+      return;
+    }
+    window.location.replace(destination);
+  }, [destination]);
+
+  if (!destination && !error) {
+    return (
+      <Box
+        height="100%"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Spinner/>
+      </Box>
+    )
+  }
+
+  return <p>{error && JSON.stringify(error)}</p>
 }
 
 export default HandleRedirect;
