902b2f4cf7facd4ede2c0375256ce2590b8ec395
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-25 13:18:23 +0700

    feat: add getDestination function

--- server/src/controller/shortUrl.controller.ts
+++ server/src/controller/shortUrl.controller.ts
@@ -5,7 +5,7 @@ import analytics from "../models/analytics.model";
 export const createShortUrl = async (req: Request, res: Response) => {
   const {destination} = req.body;
   const newUrl = await shortUrl.create({destination});
-  return res.send(newUrl);
+  return res.send(newUrl).status(201);
 }
 
 export const handleRedirect = async (req: Request, res: Response) => {
@@ -17,6 +17,17 @@ export const handleRedirect = async (req: Request, res: Response) => {
   await analytics.create({ shortUrl: resource._id })
   const {destination} = resource;
   return res.redirect(destination);
+}
+
+export const getDestination = async (req: Request, res: Response) => {
+  const {shortId} = req.params;
+  const resource = await shortUrl.findOne({shortId}).lean();
+  if (!resource) {
+    return res.sendStatus(404);
+  }
+  await analytics.create({ shortUrl: resource._id })
+  const {destination} = resource;
+  return res.send({destination}).status(200);
 }
 
 export const getAnalytics = async (_: Request, res: Response) => {
